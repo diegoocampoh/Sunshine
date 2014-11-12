06c1330243d5a6b9948451a910311274df644032
diego <diegoocampoh@gmail.com>
2014-11-12 20:53:17 -0200

    2 terminada!

--- app/src/main/java/com/example/diego/sunshine/MainActivity.java
+++ app/src/main/java/com/example/diego/sunshine/MainActivity.java
@@ -1,24 +1,9 @@
 package com.example.diego.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -27,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ForecastFragment())
